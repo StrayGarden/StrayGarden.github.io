5905168ed6ec4be9bfe7d283dc4a63ac095ec91e
StrayGarden <LengendaryGaming333@gmail.com>
2025-12-14 05:10:53 -0500

    Working on index CSS and Other Pages

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 
+
+
+// 3D JS working on index page
+// Currently not working properly 
+// 3D.js Package setup improperly?
+
+
+import './header.css';
+import './index.css';
+
+import * as THREE from 'three';
+
+//Three.js setup
+
+// Create a scene
+const scene = new THREE.Scene();
+
+// Set camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+
+// setup canvas to screen size
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
+
+
+
+
+
+// // Append the renderer's canvas element through DOM manipulation
+document.body.appendChild(renderer.domElement);
